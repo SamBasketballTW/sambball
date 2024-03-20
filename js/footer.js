2ce6783446b99075ac40a1bc3a3f1951be866701
Samuel Chow <samuel@Samuels-MacBook-Pro.local>
2024-03-20 23:49:21 +0800

    update 03/20

--- js/footer.js
+++ js/footer.js
@@ -6,9 +6,14 @@ $(document).ready(function () {
                     </a>
                     <br>
                     <a style="color:white;"
-                        href="https://www.instagram.com/sam_basketball_tw/">歡迎追蹤 
+                        href="https://www.instagram.com/sam_basketball_tw/">
                         <i class="bi bi-instagram" style="color:white; font-size: 18px;"></i>
-                         IG: sam_basketball_tw
+                        sam_basketball_tw&nbsp;
+                    </a>
+                    <a style="color:white;"
+                        href="https://www.instagram.com/sam_basketball_tw/"> 
+                        <i class="bi bi-envelope-open" style="color:white; font-size: 18px;"></i>
+                        Gmail
                     </a>
                     <br>
                     <a style="color:white; font-size:12px;">All rights reserved © 2024</a>
