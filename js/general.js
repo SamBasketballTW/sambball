86a89354aae3af3ef3e5523b9ba86084da49bec9
Samuel Chow <samuel@Samuels-MacBook-Pro.local>
2024-05-18 17:30:37 +0800

    update 5/18

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     timeStamp = document.getElementById("timeStamp");
-    timeStamp.innerHTML = `<b>最後更新: 2024.05.17</b>`
+    timeStamp.innerHTML = `<b>最後更新: 2024.05.18 17:00</b>`
 
     test = ""
     // test = ".html"
