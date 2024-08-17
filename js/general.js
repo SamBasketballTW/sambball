0b007b3eca139ca0cb68d3f41ee256beffefc8bc
Samuel Chow <sambasketballtw@gmail.com>
2024-08-17 21:23:47 +0800

    update 8/17

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     timeStamp = document.getElementById('timeStamp');
-    timeStamp.innerHTML = `<b>最後更新: 2024.08.16 20:30</b>`
+    timeStamp.innerHTML = `<b>最後更新: 2024.08.17 17:00</b>`
 
     test = ''
     // test = '.html'
