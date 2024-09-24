b5cbbbaf0d58f2b7bb40611ae0abf363fd55a5e0
Samuel Chow <sambasketballtw@gmail.com>
2024-09-25 03:01:26 +0800

    update 9/24

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     timeStamp = document.getElementById('timeStamp');
-    timeStamp.innerHTML = `<b>最後更新: 2024.09.20 17:00</b>`
+    timeStamp.innerHTML = `<b>最後更新: 2024.09.21 17:00</b>`
 
     test = ''
     // test = '.html'
@@ -27,17 +27,11 @@ $(document).ready(function () {
     <div class="container" style="padding:1.5rem;text-align:center;">
         <a style="color:white;">
             山姆籃球
-        </a>
-        <br>
+        </a>&nbsp;
         <a style="color:white;"
             href="https://www.instagram.com/sam_basketball_tw/" target="_blank">
             <i class="bi bi-instagram" style="color:white; font-size: 18px;"></i>
             sam_basketball_tw&nbsp;
-        </a>
-        <a style="color:white;"
-            href="mailto:sambasketballtw@gmail.com"> 
-            <i class="bi bi-envelope-open" style="color:white; font-size: 18px;"></i>
-            Gmail
         </a>
         <br>
         <a style="color:white; font-size:12px;">All rights reserved © 2024</a>
@@ -69,7 +63,7 @@ braves = new Team('men', 'plg', 'braves', '臺北富邦勇士', '勇士', '執�
 pilots = new Team('men', 'plg', 'pilots', '桃園璞園領航猿', '領航猿', '總教練: 卡米諾斯 Iurgi Caminos');
 ghosthawks = new Team('men', 'plg', 'ghosthawks', '臺南台鋼獵鷹', '獵鷹', '總教練: 柯納 Raoul Korner');
 steelers = new Team('men', 'plg', 'steelers', '高雄17直播鋼鐵人', '鋼鐵人', '總教練: 邱大宗');
-mars = new Team('men', 'tpbl', 'mars', '臺北戰神', '戰神', '總教練: 許皓程');
+mars = new Team('men', 'tpbl', 'mars', '臺北台新戰神', '戰神', '總教練: 許皓程');
 kings = new Team('men', 'tpbl', 'kings', '新北國王', '國王', '總教練: 萊恩 Ryan Marchand');
 dea = new Team('men', 'tpbl', 'dea', '新北中信特攻', '特攻', '總教練: 李逸驊');
 leopards = new Team('men', 'tpbl', 'leopards', '桃園台啤永豐雲豹', '雲豹', '總教練: 查爾斯 Charles Dubé-Brais');
