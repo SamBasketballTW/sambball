bc5221222ed25688cbaad8a79b2a23c5ff8dbfeb
samuelboyy11 <samuelboyy11@gmail.com>
2024-12-02 20:16:30 +0800

    update 12/2

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     timeStamp = document.getElementById('timeStamp');
-    timeStamp.innerHTML = `<b>最後更新: 2024.11.30</b>`
+    timeStamp.innerHTML = `<b>最後更新: 2024.12.02</b>`
 
     test = ''
     // test = '.html'
